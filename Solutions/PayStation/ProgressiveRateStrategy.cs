560cd19043cb4f346be3ba32581f5401fd07efdc
Simon Søllingvraa <simon@soellingvraa.dk>
2014-09-18 20:42:18 +0200

    implemented the progressive rate strategy

--- Solutions/PayStation/ProgressiveRateStrategy.cs
+++ Solutions/PayStation/ProgressiveRateStrategy.cs
@@ -1,10 +1,30 @@
-﻿namespace PayStation
+﻿using System;
+
+namespace PayStation
 {
     public class ProgressiveRateStrategy : ICalculateRateStrategy
     {
         public int CalculateRate(int coinValue)
         {
-            return coinValue * 7;
+            var remainingCoinValue = coinValue;
+            var accumulatedMinutes = 0;
+
+            if (350 < remainingCoinValue)
+            {
+                accumulatedMinutes += (remainingCoinValue - 350)/5*1;
+                remainingCoinValue = 350;
+            }
+            if (150 < remainingCoinValue && remainingCoinValue <= 350)
+            {
+                accumulatedMinutes += Convert.ToInt32((remainingCoinValue - 150)/5*1.5);
+                remainingCoinValue = 150;
+            }
+            if (remainingCoinValue <= 150)
+            {
+                accumulatedMinutes += (remainingCoinValue - 150) / 5 * 2;
+            }
+
+            return accumulatedMinutes;
         }
     }
 }
