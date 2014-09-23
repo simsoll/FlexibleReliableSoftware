dea21090aca0e4f6d789b958312b9bba5fc7ca2a
Simon Søllingvraa <simon@soellingvraa.dk>
2014-09-23 19:29:28 +0200

    Added interface implementations
    Added implementations of the IWeekendDecisionStrategy

---
+++ Solutions/PayStation/ClockBasedDecisionStrategy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PayStation
+{
+    public class ClockBasedDecisionStrategy : IWeekendDecisionStrategy
+    {
+        public bool IsWeekend()
+        {
+            var dayOfWeek = DateTime.Now.DayOfWeek;
+
+            return dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sunday;
+        }
+    }
+}
