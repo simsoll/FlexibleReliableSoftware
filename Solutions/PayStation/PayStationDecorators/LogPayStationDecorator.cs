0eb5c053f571bd3be90f55d2923edbaf0dd97388
Simon Søllingvraa <simon@soellingvraa.dk>
2014-09-26 20:51:05 +0200

    Added Exercise 20.3

---
+++ Solutions/PayStation/PayStationDecorators/LogPayStationDecorator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayStation;
+
+namespace Exercise5._2.Decorators
+{
+    public class LogPayStationDecorator : IPayStation
+    {
+        private readonly IPayStation _payStation;
+
+        public LogPayStationDecorator(IPayStation payStation)
+        {
+            _payStation = payStation;
+        }
+
+        public void AddPayment(int coinValue)
+        {
+            Console.WriteLine("{0} : {1}", coinValue, DateTime.Now);
+            _payStation.AddPayment(coinValue);
+        }
+
+        public int ReadDisplay()
+        {
+            return _payStation.ReadDisplay();
+        }
+
+        public IReceipt Buy()
+        {
+            return _payStation.Buy();
+        }
+
+        public IDictionary<int, int> Cancel()
+        {
+            return _payStation.Cancel();
+        }
+
+        public void ReConfigure(IPayStationFactory payStationFactory)
+        {
+            _payStation.ReConfigure(payStationFactory);
+        }
+    }
+}
