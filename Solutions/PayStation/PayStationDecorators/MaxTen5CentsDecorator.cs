2f11c1f4f6f48c08cbc10aba11deda04b4494c25
Simon Søllingvraa <simon@soellingvraa.dk>
2014-09-26 20:51:20 +0200

    Added Exercise 20.4

---
+++ Solutions/PayStation/PayStationDecorators/MaxTen5CentsDecorator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PayStation;
+
+namespace Exercise5._2.PayStationDecorators
+{
+    public class MaxTen5CentsDecorator : IPayStation
+    {
+        private readonly IPayStation _payStation;
+        private int _numberOfFiveCents;
+
+        public MaxTen5CentsDecorator(IPayStation payStation)
+        {
+            _payStation = payStation;
+        }
+
+        public void AddPayment(int coinValue)
+        {
+            if (coinValue == 5)
+            {
+                _numberOfFiveCents++;
+            }
+
+            if (_numberOfFiveCents > 10)
+            {
+                _numberOfFiveCents = 0;
+                _payStation.Cancel();
+                return;
+            }
+
+            _payStation.AddPayment(coinValue);
+        }
+
+        public int ReadDisplay()
+        {
+            return _payStation.ReadDisplay();
+        }
+
+        public IReceipt Buy()
+        {
+            return _payStation.Buy();
+        }
+
+        public IDictionary<int, int> Cancel()
+        {
+            return _payStation.Cancel();
+        }
+
+        public void ReConfigure(IPayStationFactory payStationFactory)
+        {
+            _payStation.ReConfigure(payStationFactory);
+        }
+    }
+}
